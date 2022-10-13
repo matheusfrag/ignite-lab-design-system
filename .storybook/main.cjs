437b7508155f1375596e9c4c6f520855789827a6
Matheus <matheusfrag@gmail.com>
2022-10-12 21:57:12 -0300

    main.cjs

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -3,7 +3,8 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y'
   ],
   framework: '@storybook/react',
   core: {
@@ -14,7 +15,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/ignite-lab-desing-system/'
+      config.base = '/ignite-lab-design-system/'
     }
 
     return config
